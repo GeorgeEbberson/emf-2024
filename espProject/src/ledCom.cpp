e98cc7cd758b352a9769c585444ffb4e626b500f
Max Riddings <MLRiddings@googlemail.com>
2024-05-31 21:01:02 +0100

    WORKING!!!!

---
+++ espProject/src/ledCom.cpp
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+#include <FastLED.h>
+
+#define DATA_PIN 26
+#define NUM_LEDS 600
+
+CRGB leds[NUM_LEDS];
+
+void setLed(CRGB *led, int r, int g, int b)
+{
+    led->r = r;
+    led->g = g;
+    led->b = b;
+}
+
+void ledsetup()
+{
+    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+    FastLED.setCorrection(LEDColorCorrection::TypicalPixelString);
+    setLed(&leds[0], 255, 0, 0);
+    setLed(&leds[1], 0, 255, 0);
+    setLed(&leds[2], 0, 0, 255);
+    FastLED.show();
+
+}
+
+void ledset(CRGB *color)
+{
+    // Set all LEDs to selected color
+    for (int i = 1; i < NUM_LEDS; i++)
+    {
+        leds[i] = *color;
+    }
+
+    // Tick First LED as a keepalive
+    if (leds[0] == 0)
+    {
+        leds[0] = *color;
+    }
+    else
+    {
+        leds[0] = 0;
+    }
+    // Serial.printf("Wang: R: %u G: %u B: %u into LEDs\n", color->r, color->g, color->b);
+    FastLED.show();
+}
---
+++ espProject/src/ledCom.hpp
@@ -0,0 +1,9 @@
+#ifndef LEDCOM
+#define LEDCOM
+
+#include <FastLED.h>
+
+void ledsetup();
+void ledset(CRGB *color);
+
+#endif // LEDCOM
