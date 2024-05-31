01591b4a53fe9a23faf1674df50d191c8ef21678
Max Riddings <MLRiddings@googlemail.com>
2024-05-31 21:01:02 +0100

    WORKING!!!!

--- espProject/src/main.cpp
+++ espProject/src/main.cpp
@@ -1,27 +1,19 @@
+
 #include <Arduino.h>
 #include <FastLED.h>
 
-#define DATA_PIN 26
-#define NUM_LEDS 600
-#define COLOR_NUM 5
+#include "wifiCom.hpp"
+#include "ledCOM.hpp"
 
-CRGB leds[NUM_LEDS];
-CRGB colors[COLOR_NUM] = {CRGB::White, CRGB::Black, CRGB::Red, CRGB::Green, CRGB::Blue};
-
-void setup()
-{
-    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+void setup() {
+    Serial.begin(9600);
+    ledsetup();
+    Serial.print("Setup done");
 }
 
-void loop()
-{
-    static int idx = 0;
-
-    for (int i = 0; i < NUM_LEDS; i++)
-    {
-        leds[i] = colors[idx];
-    }
-    idx = (idx + 1) % COLOR_NUM;
-    FastLED.show();
+void loop() {
+    CRGB color;
+    loopWiFi(&color);
+    ledset(&color);
     delay(1000);
 }
