f1758ee1ea226dd1312b44a3c4389d3a40bc2cb2
George Ebberson <george.ebberson@gmail.com>
2024-06-01 20:46:24 +0100

    Use enum members instead of hard constants

---
+++ espProject/src/common.hpp
@@ -0,0 +1,25 @@
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+#include <stdint.h>
+
+#define VEC3 (3u) // Num elements in 3-vector
+
+// Numbers hardcoded to match the cloud-side
+typedef enum {
+    Mode_None = 0u,
+    Mode_Normal = 1,
+    Mode_Rave = 2,
+    Mode_Pulse = 3,
+    Mode_Spectrum = 4,
+    Mode_Rainbow = 5,
+    Mode_MAX
+} Mode_t;
+
+typedef struct ThreadMsg_s {
+    Mode_t mode;
+    uint8_t rgb[VEC3];
+    uint8_t brightness;
+} ThreadMsg_t;
+
+#endif // COMMON_HPP
